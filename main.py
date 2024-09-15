64eb065846dbaac9d37ac18b82608f1f725f8202
Nikhil-Wui <nikhilpie12@gmail.com>
2024-09-16 02:09:28 +0530

    Add files via upload
    Just the first project of stone, paper and scissor game as i haven't played snake, water and gun game in the tutorial of Code With Harry.

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+
+computer = random.choice([0, -1, 1])
+
+you= input("Enter Your Choice: ")
+youdict={"s":1,"p":-1,"z":0}
+
+reversedict={1:"Stone",-1:"Paper",0:"Scissor"}
+
+youstr=youdict[you]
+print(f"Computer choose {reversedict[computer]} and you choosed {reversedict[youstr]}")
+
+if(computer==youstr):
+    print("It's a DRAW")
+else:
+    if(computer==1 and youstr==-1):
+        print("You win")
+    elif(computer==1 and youstr==0):
+        print("You Lost")
+    elif(computer==-1 and youstr==0):
+        print("You win")
+    elif(computer==-1 and youstr==1):
+        print("You Lost")
+    elif(computer==0 and youstr==-1):
+        print("You Lost")
+    elif(computer==0 and youstr==1):
+        print("You win")
+
+
+        
+    
+
+
+
